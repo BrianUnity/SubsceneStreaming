efa9da834149832f8303e9b72a05eca18b6c7da2
Brian <brian.oconnor@unity3d.com>
2020-06-04 17:29:11 +0100

    Very simple subscene streaming setup

---
+++ Assets/Scripts/SubSceneLoader.cs
@@ -0,0 +1,46 @@
+﻿using Unity.Entities;
+using Unity.Scenes;
+using UnityEngine;
+using Hash128 = Unity.Entities.Hash128;
+
+public class SubSceneLoader : ComponentSystem
+{
+    private SceneSystem sceneSys;
+
+    protected override void OnCreate()
+    {
+        sceneSys = World.GetOrCreateSystem<SceneSystem>();
+    }
+    
+    protected override void OnUpdate()
+    {
+        SubSceneMap map = GameObject.FindObjectOfType<SubSceneMap>();
+        
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            ToggleScene(map.Spheres);
+        }
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            ToggleScene(map.Cubes);
+        }
+    }
+
+    void ToggleScene(in SubScene scene)
+    {
+        if (IsSceneLoaded(scene.SceneGUID))
+        {
+            sceneSys.UnloadScene(scene.SceneGUID);
+        }
+        else
+        {
+            sceneSys.LoadSceneAsync(scene.SceneGUID);
+        }
+    }
+    
+    bool IsSceneLoaded(in Hash128 sceneGUID)
+    {
+        var sceneEntity = sceneSys.GetSceneEntity(sceneGUID);
+        return sceneSys.IsSceneLoaded(sceneEntity);
+    }
+}
---
+++ Assets/Scripts/SubSceneMap.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Scenes;
+using UnityEngine;
+
+public class SubSceneMap : MonoBehaviour
+{
+    public SubScene Cubes;
+    public SubScene Spheres;
+}
